ed5cd488d8c4b7a1a9caa17cb10743599f2f7bb5
shiva508 <dasarishiva1@gmail.com>
2024-07-24 14:09:15 +0530

    activemq transmitter-receiver

---
+++ src/main/java/com/comrade/configuration/StudentpoolEmailJmsConfiguration.java
@@ -0,0 +1,54 @@
+package com.comrade.configuration;
+
+import jakarta.jms.JMSException;
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import com.comrade.configuration.properties.StudentpoolProperties;
+
+@Configuration
+public class StudentpoolEmailJmsConfiguration {
+	
+	@Autowired
+	private StudentpoolProperties studentpoolProperties;
+
+	@Bean
+	public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() throws JMSException {
+		DefaultJmsListenerContainerFactory jmsListenerContainerFactory = new DefaultJmsListenerContainerFactory();
+		jmsListenerContainerFactory.setConnectionFactory(activeMQConnectionFactory());
+		jmsListenerContainerFactory.setConcurrency("1-1");
+		return jmsListenerContainerFactory;
+	}
+
+	@Bean
+	public ActiveMQConnectionFactory activeMQConnectionFactory() throws JMSException {
+		ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
+		activeMQConnectionFactory.setBrokerURL(studentpoolProperties.getJmsUrl());
+		activeMQConnectionFactory.setUser(studentpoolProperties.getUserName());
+		activeMQConnectionFactory.setPassword(studentpoolProperties.getPassword());
+		return activeMQConnectionFactory;
+	}
+	
+	@Bean
+	public JmsTemplate jmsTemplate() throws JMSException {
+		JmsTemplate jmsTemplate=new JmsTemplate();
+		jmsTemplate.setConnectionFactory(activeMQConnectionFactory());
+		jmsTemplate.setMessageConverter(jacksonJmsMessageConverter());
+		return jmsTemplate;
+	}
+	
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
+}
---
+++ src/main/java/com/comrade/jms/receiver/JmsReceiver.java
@@ -0,0 +1,8 @@
+package com.comrade.jms.receiver;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JmsReceiver {
+	
+}
