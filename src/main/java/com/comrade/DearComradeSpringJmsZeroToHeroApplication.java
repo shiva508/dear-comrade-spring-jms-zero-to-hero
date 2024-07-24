273a4029c86cbe32a69b94a453dfeb23807d87d3
shiva508 <dasarishiva1@gmail.com>
2024-07-24 13:43:03 +0530

    initial commit

---
+++ src/main/java/com/comrade/DearComradeSpringJmsZeroToHeroApplication.java
@@ -0,0 +1,13 @@
+package com.comrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearComradeSpringJmsZeroToHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearComradeSpringJmsZeroToHeroApplication.class, args);
+	}
+
+}
